d148afdf8635454f4957e230e55bd33809e11811
SpokuLapsa <109891541+SpokuLapsa@users.noreply.github.com>
2023-04-07 19:04:29 +0700

    Add files via upload

--- minimap.js
+++ minimap.js
@@ -1,4 +1,4 @@
-const VERSION = 14
+const VERSION = 15
 
 const coordsEl = document.querySelector('div[title="Координаты пикселя."]')
 
@@ -16,7 +16,7 @@ let imgCtx, temCtx, imgW, imgH;
 
 async function loadTemplate(){
     return new Promise((res, rej) => {
-        const link = 'https://i.imgur.com/zJSlgo8.png';
+        const link = 'https://i.imgur.com/5C8qEuR.png';
         const img = new Image();
 
         img.crossOrigin = 'anonymous';
